117ff3cd191463066ddacdb2ebd40ee4c6ff2810
Eric Toporek <etoporek98@gmail.com>
2026-03-08 16:37:55 -0600

    feat: Exception handling (P4)

--- src/main/java/com/product/api/controller/CtrlCategory.java
+++ src/main/java/com/product/api/controller/CtrlCategory.java
@@ -3,6 +3,7 @@ package com.product.api.controller;
 import com.product.api.entity.Category;
 import com.product.api.service.SvcCategory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,7 @@ public class CtrlCategory {
     private SvcCategory svcCategory;
 
     @GetMapping
-    public List<Category> getCategories() {
+    public ResponseEntity<List<Category>> getCategories() {
         return svcCategory.getCategories();
     }
 }
