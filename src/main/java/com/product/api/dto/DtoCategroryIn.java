1ef1a886c4abcd2aaf99a409997dfdeb7cd0a7a3
Eric Toporek <44303760+toporaku@users.noreply.github.com>
2026-03-25 00:23:43 -0600

    Update src/main/java/com/product/api/dto/DtoCategroryIn.java
    Co-authored-by: Mónica Miranda <53272126+monmm@users.noreply.github.com>

--- src/main/java/com/product/api/dto/DtoCategroryIn.java
+++ src/main/java/com/product/api/dto/DtoCategroryIn.java
@@ -33,13 +33,6 @@ public class DtoCategroryIn {
         this.tag = tag;
     }
 
-    public Integer getStatus() {
-        return status;
-    }
-
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
 
     @Override
     public String toString() {
