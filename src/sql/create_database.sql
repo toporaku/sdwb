3a4ca86862c955fc1cd49e964222ac74b62e2198
Eric Toporek <etoporek98@gmail.com>
2026-04-20 20:05:25 -0600

    feat: implement product image management with database support and file upload functionality

--- src/sql/create_database.sql
+++ src/sql/create_database.sql
@@ -32,3 +32,33 @@ INSERT IGNORE INTO category (category_id, category, tag, status) VALUES
 (2, 'Relojes', 'Rljs', 1),
 (3, 'Mochilas', 'Mchs', 1),
 (4, 'Zapatos', 'Zpts', 0);
+
+-- 6. Create product table
+DROP TABLE IF EXISTS product_image;
+DROP TABLE IF EXISTS product;
+
+CREATE TABLE product(
+    product_id INT NOT NULL AUTO_INCREMENT,
+    gtin CHAR(13) NOT NULL,
+    product VARCHAR(100) NOT NULL,
+    description TEXT,
+    price FLOAT NOT NULL,
+    stock INT NOT NULL,
+    category_id INT NOT NULL,
+    status TINYINT NOT NULL,
+    PRIMARY KEY (product_id),
+    CONSTRAINT fk_product_category FOREIGN KEY (category_id) REFERENCES category(category_id)
+);
+
+CREATE UNIQUE INDEX ux_product_gtin ON product(gtin);
+CREATE UNIQUE INDEX ux_product_product ON product(product);
+
+-- 7. Create product_image table
+CREATE TABLE product_image(
+	product_image_id INT NOT NULL AUTO_INCREMENT,
+    product_id INT NOT NULL,
+    image VARCHAR(255) NOT NULL,
+    status TINYINT NOT NULL,
+    PRIMARY KEY (product_image_id),
+    CONSTRAINT fk_product_image_product FOREIGN KEY (product_id) REFERENCES product(product_id)
+);
